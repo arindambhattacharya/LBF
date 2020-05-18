a27a6bc398f6afe0de872572cc88a1d72d4c56de
Arindam Bhattacharya <arindambhattacharya@protonmail.com>
2020-05-18 11:01:17 +0530

    Don't share Y

--- facebook/journal_experiments.py
+++ facebook/journal_experiments.py
@@ -287,6 +287,12 @@ if __name__ == "__main__":
         id_vars=["Method", "Batch", "Run"], value_vars=["FPS", "Time", "Memory"]
     )
     g = sns.relplot(
-        "Batch", "value", col="variable", hue="Method", kind="line", data=melted_df
+        "Batch",
+        "value",
+        col="variable",
+        hue="Method",
+        kind="line",
+        data=melted_df,
+        sharey=False,
     )
     g.savefig("plots/fb_metrics.png")
