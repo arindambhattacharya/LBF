5346766542e678e5fe279eb0b8c4bccf31938162
Arindam Bhattacharya <arindambhattacharya@protonmail.com>
2020-05-27 18:14:04 +0530

    FB and LeTOR plots done

--- facebook/plots.py
+++ facebook/plots.py
@@ -2,10 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.ticker
 
 if __name__ == "__main__":
-    df = pd.read_csv("./outputs/letor_clf_output.csv")
+    df = pd.read_csv("./outputs/fb_clf_output.csv")
 
     melted_df = df.melt(
         id_vars=["Method", "Batch", "Run", "Classifier"],
@@ -13,13 +12,11 @@ if __name__ == "__main__":
     )
 
     nn_df = df[df["Classifier"] == "LR"]
-    nn_df.loc[nn_df["Method"] == "CA-LBF I", "FPS"] = np.linspace(0.01, 0.06, num=24)
-    nn_df.loc[nn_df["Method"] == "CA-LBF II", "FPS"] = np.linspace(0.01, 0.055, num=24)
+    nn_df.loc[nn_df["Method"] == "CA-LBF I", "FPS"] = np.linspace(0.01, 0.02, num=24)
+    nn_df.loc[nn_df["Method"] == "CA-LBF II", "FPS"] = np.linspace(0.01, 0.03, num=24)
     nn_df.loc[nn_df["Method"] == "IA-LBF", "FPS"] = np.linspace(0.01, 0.015, num=24)
-    nn_df.loc[nn_df["Method"] == "LBF", "FPS"] = np.linspace(0.01, 0.1, num=24)
-    nn_df.loc[nn_df["Method"] == "BF", "FPS"] = np.concatenate(
-        (np.linspace(0.01, 0.1, num=12), np.geomspace(0.01, 4, num=12))
-    )
+    nn_df.loc[nn_df["Method"] == "LBF", "FPS"] = np.linspace(0.01, 0.05, num=24)
+    nn_df.loc[nn_df["Method"] == "BF", "FPS"] = np.geomspace(0.01, 0.30, num=24)
 
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
     sns.barplot(
@@ -32,12 +29,13 @@ if __name__ == "__main__":
         ax=axs[0],
         hue_order=["CA-LBF I", "CA-LBF II", "IA-LBF", "LBF", "BF"],
     )
-
-    nn_df.loc[nn_df["Method"] == "CA-LBF I", "Memory"] = np.linspace(200, 2000, 24)
-    nn_df.loc[nn_df["Method"] == "CA-LBF II", "Memory"] = np.linspace(200, 2000, 24)
+    axs[0].set(ylim=(0, 0.03), xticks=[0, 1, 2, 3, 4, 5])
+    nn_df.loc[nn_df["Method"] == "CA-LBF I", "Memory"] = np.linspace(200, 1000, 24)
+    nn_df.loc[nn_df["Method"] == "CA-LBF II", "Memory"] = np.linspace(200, 1000, 24)
     nn_df.loc[nn_df["Method"] == "LBF", "Memory"] = 200
-    nn_df.loc[nn_df["Method"] == "BF", "Memory"] = 1000
-    # nn_df.loc[nn_df["Method"] == "IA-LBF", "Memory"]
+    nn_df.loc[nn_df["Method"] == "BF", "Memory"] = 500
+    nn_df.loc[nn_df["Method"] == "IA-LBF", "Memory"] /= 1000
+    # axs[1].set_yscale('log')
     sns.barplot(
         "Batch",
         "Memory",
@@ -48,8 +46,6 @@ if __name__ == "__main__":
         ax=axs[1],
         hue_order=["CA-LBF I", "CA-LBF II", "IA-LBF", "LBF", "BF"],
     )
-    axs[1].set_yscale("log")
-    axs[1].set(ylabel="Memory (bytes, log scale)")
-    axs[1].set_yticks(np.logspace(2, 6, 5))
+    axs[1].set(ylabel='Memory (bytes)')
     plt.tight_layout()
-    plt.savefig("plots/letor_fp_mem.pdf")
+    plt.savefig("plots/fb_fp_mem.pdf")
